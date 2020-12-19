af39865a975edd165928f4770e4d644dde9c92f8
FrankCCCCC <unaxultrapaceos5@gmail.com>
2020-12-20 00:27:42 +0800

    share memory done

---
+++ hw4-1/prof.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+echo -e "Compiling..."
+# nvcc  -O3 -std=c++11 -Xptxas=-v -arch=sm_61  -lm hw4-1_corr.cu -o ./execs/hw4-1_corr
+nvcc  -O3 -std=c++11 -Xptxas=-v -arch=sm_61  -lm hw4-1.cu -o ./execs/hw4-1
+
+rm prof/prof.nvvp
+# srun -p prof -N1 -n1 --gres=gpu:1 nvprof --metrics gld_throughput -o prof/prof.nvvp ./execs/hw4-1_corr /home/pp20/share/hw4-1/cases/c01.1 ./out/c01.1.out
+srun -p prof -N1 -n1 --gres=gpu:1 nvprof --metrics gld_throughput -o prof/prof.nvvp ./execs/hw4-1 /home/pp20/share/hw4-1/cases/c21.1 ./out/c21.1.out
